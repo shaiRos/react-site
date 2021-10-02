cd33241a90b47d6076675f0c1432f5f4599e90e7
shainamae98 <shaina.mrosell@gmail.com>
2021-10-02 17:52:17 -0600

    Updates

---
+++ precache-manifest.50835dd72d9b3df61739390d5a0b3ad3.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8219cf620caf2dfc22f1b786d2e239e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "308f15c94499009a44b4",
+    "url": "./static/css/2.b4f72424.chunk.css"
+  },
+  {
+    "revision": "c442636e1bcc60dba4b1",
+    "url": "./static/css/main.6e6d88ea.chunk.css"
+  },
+  {
+    "revision": "308f15c94499009a44b4",
+    "url": "./static/js/2.53cb667a.chunk.js"
+  },
+  {
+    "revision": "14630c7d0acd9ab06b97ff38ad7d75df",
+    "url": "./static/js/2.53cb667a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c442636e1bcc60dba4b1",
+    "url": "./static/js/main.e1427332.chunk.js"
+  },
+  {
+    "revision": "1039c400d893d0f64a04",
+    "url": "./static/js/runtime-main.4d704fb3.js"
+  },
+  {
+    "revision": "23b806abd142e1ce77c551ca11a87352",
+    "url": "./static/media/API.23b806ab.png"
+  },
+  {
+    "revision": "1efcca1132b1788b700e546eb4e4687c",
+    "url": "./static/media/AboutBG.1efcca11.png"
+  },
+  {
+    "revision": "4ed62a9e345eb04479485f43f6c38c86",
+    "url": "./static/media/Client-Menu.4ed62a9e.png"
+  },
+  {
+    "revision": "bcd58493308e58b0756b533394d74c1a",
+    "url": "./static/media/Course Postings.bcd58493.jpg"
+  },
+  {
+    "revision": "8512c30358966b813795e555d20312f3",
+    "url": "./static/media/Cover.8512c303.jpg"
+  },
+  {
+    "revision": "d24217950191afe514bdde3ab6192643",
+    "url": "./static/media/ERD.d2421795.png"
+  },
+  {
+    "revision": "0ac0bb0c4d5c08b8856796efd586bd95",
+    "url": "./static/media/HCI II.0ac0bb0c.png"
+  },
+  {
+    "revision": "bb843f30e752189b4b723c96235434b9",
+    "url": "./static/media/Maps.bb843f30.jpg"
+  },
+  {
+    "revision": "70d3d553c9bac1c0258b0dc916d17243",
+    "url": "./static/media/Master-Server start.70d3d553.png"
+  },
+  {
+    "revision": "3d50b28422d99260fe16cd28e9c760f6",
+    "url": "./static/media/Other Projects.3d50b284.png"
+  },
+  {
+    "revision": "94e96ea0d5b64e825540e1b292710022",
+    "url": "./static/media/Pic1.94e96ea0.jpg"
+  },
+  {
+    "revision": "4d0ebd972ce9375be8c2a605f6b15044",
+    "url": "./static/media/ProfilePic.4d0ebd97.jpg"
+  },
+  {
+    "revision": "36b4e968541109a46a0f62559ac283da",
+    "url": "./static/media/RM.36b4e968.png"
+  },
+  {
+    "revision": "d42f7d5de2d3b3a63aab6fd301537e5c",
+    "url": "./static/media/Requests.d42f7d5d.jpg"
+  },
+  {
+    "revision": "216d89072199ce53c6901ff7a8a5dad3",
+    "url": "./static/media/Restaurant API.216d8907.png"
+  },
+  {
+    "revision": "3a2949fec4b019bf49a3ee0efe82556b",
+    "url": "./static/media/Scholarship System.3a2949fe.png"
+  },
+  {
+    "revision": "2385885c3de1e55f89491a768b7a7ded",
+    "url": "./static/media/ScholarshipLogin.2385885c.png"
+  },
+  {
+    "revision": "ce388d33af3d39b106e1b8c33fad7b11",
+    "url": "./static/media/ShainaR_Resume.ce388d33.pdf"
+  },
+  {
+    "revision": "b281534febf0658ddf6f464441deb726",
+    "url": "./static/media/TutorFinder.b281534f.png"
+  },
+  {
+    "revision": "c70ebe22aff4e20bb19a6947e145553b",
+    "url": "./static/media/TutorFinderFront.c70ebe22.png"
+  },
+  {
+    "revision": "03f43d6073921197e73eb150844adfb9",
+    "url": "./static/media/Vid.03f43d60.mov"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "8dfb5c673f8b86c04c1c25b916b13431",
+    "url": "./static/media/client-chained-example.8dfb5c67.png"
+  },
+  {
+    "revision": "e407298ad5a8f8b699072b07f1a772cd",
+    "url": "./static/media/client-reverse-example.e407298a.png"
+  },
+  {
+    "revision": "fcd0636482c954967b8233e6858cc579",
+    "url": "./static/media/holder.fcd06364.jpg"
+  },
+  {
+    "revision": "cc5c0f22bba441fb1d6c9d957ab21c4f",
+    "url": "./static/media/icon.cc5c0f22.png"
+  },
+  {
+    "revision": "76753e597fdbfdd663c8707f675738ce",
+    "url": "./static/media/loginLogo.76753e59.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "e24d234353756447286fc118ba121b30",
+    "url": "./static/media/project1_finalDecision.e24d2343.png"
+  },
+  {
+    "revision": "031dacbddd20c613f611924144becb44",
+    "url": "./static/media/project1_first10PickedSketches.031dacbd.png"
+  },
+  {
+    "revision": "7a1f8250d6782fd44a7af825a76c73f5",
+    "url": "./static/media/project1_flowers.7a1f8250.png"
+  },
+  {
+    "revision": "413d0b49e483de09b0cb61fd1a87516b",
+    "url": "./static/media/project1_myInitialSketches.413d0b49.png"
+  },
+  {
+    "revision": "922d54d10a445f6ac8c984fb58153328",
+    "url": "./static/media/project1_round2Sketches.922d54d1.png"
+  },
+  {
+    "revision": "8dccb931046d29a30393dad814e877e8",
+    "url": "./static/media/project1_round3Sketches.8dccb931.png"
+  },
+  {
+    "revision": "e665c1ffec61274b7493dc7f8b28d52e",
+    "url": "./static/media/sketches_refinement1.e665c1ff.png"
+  },
+  {
+    "revision": "e5e7f06c41ab0308633c55e2714a2b8b",
+    "url": "./static/media/sketches_refinement10.e5e7f06c.png"
+  },
+  {
+    "revision": "172c2019e45d94ccf5c57bfa810940ed",
+    "url": "./static/media/sketches_refinement2.172c2019.png"
+  },
+  {
+    "revision": "ee45fd79a106e804c2dbaffc7002c0ea",
+    "url": "./static/media/sketches_refinement3.ee45fd79.jpg"
+  },
+  {
+    "revision": "705f49f9d402655c63c18b33adc37727",
+    "url": "./static/media/sketches_refinement4.705f49f9.png"
+  },
+  {
+    "revision": "e6b81828315784965e4343295d677c0a",
+    "url": "./static/media/sketches_refinement5.e6b81828.png"
+  },
+  {
+    "revision": "ac22e17e029e96ea107a1ef24fb47737",
+    "url": "./static/media/sketches_refinement6.ac22e17e.png"
+  },
+  {
+    "revision": "98061430f0ce04364b07c80f04792321",
+    "url": "./static/media/sketches_refinement7.98061430.jpg"
+  },
+  {
+    "revision": "6e0d458f349d904e79f10818e28fcc5c",
+    "url": "./static/media/sketches_refinement8.6e0d458f.jpg"
+  },
+  {
+    "revision": "fdbc0deffc13355e8771c7683702e3eb",
+    "url": "./static/media/sketches_refinement9.fdbc0def.png"
+  },
+  {
+    "revision": "36ed4d38a183693ac3c58524b37be22d",
+    "url": "./static/media/wip.36ed4d38.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bb224f6cc5b260b89e5bb614c3d0d5a5.js"
+  "./precache-manifest.50835dd72d9b3df61739390d5a0b3ad3.js"
 );
 
 self.addEventListener('message', (event) => {
